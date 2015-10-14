c2bc8a156d1be410ef3d01432850d904569e3eec
OlalKeith <jkeneth.jk@gmail.com>
2015-10-14 15:13:44 +0300

    forms

--- ceaser/home/views.py
+++ ceaser/home/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import Ceaser
 # Create your views here.
 
 
 def index(request):
-	return render(request, 'index.html' ,{})
+	form = Ceaser(request.POST or None)
+	context = {
+		'form':form
+	}
 	
+	result = ceaser_cipher(input)
+	return render(request, 'index.html' , context)
+
+def ceaser_cipher(plaintext):
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	num = 2
+
+	cipher = ''
+	for c in plaintext:
+		if c.isalpha():
+			cipher += alphabet[(alphabet.index(c)+ num)]
+		else:
+			cipher+= c
+	return cipher
